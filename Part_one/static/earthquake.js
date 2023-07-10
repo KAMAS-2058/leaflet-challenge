8bd870a2b6062e70332f58841ed7af9f72c44e9d
Kyle <kyle.stephens@ucdenver.edu>
2023-07-10 15:29:18 -0600

    making needed files

---
+++ Part_one/static/earthquake.js
@@ -0,0 +1,14 @@
+// api call all earthquakes for the past 7 days ~~ past 30 days was having trouble
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+
+
+
+
+
+
+
+
+
+
+
